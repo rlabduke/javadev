f1300b09bf50d3e40930df2dd4f97ca58d837c73
iwd <iwd@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2004-04-09 15:51:22 +0000

    Added lib/ and makedocs as a test of the subversion repository.

---
+++ trunk/javadev/lib/templates/template.java
@@ -0,0 +1,169 @@
+// (jEdit options) :folding=explicit:collapseFolds=1:
+//{{{ Package, imports
+package |PACKAGE|;
+
+//import java.awt.*;
+//import java.awt.event.*;
+import java.io.*;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.*;
+//import java.util.regex.*;
+//import javax.swing.*;
+//import driftwood.*;
+//}}}
+/**
+* <code>|CLASSNAME|</code> has not yet been documented.
+*
+* <p>Copyright (C) |YEAR| by Ian W. Davis. All rights reserved.
+* <br>Begun on |DATE|
+*/
+public class |CLASSNAME| //extends ... implements ...
+{
+//{{{ Constants
+//}}}
+
+//{{{ Variable definitions
+//##############################################################################
+//}}}
+
+//{{{ Constructor(s)
+//##############################################################################
+    public |CLASSNAME|()
+    {
+        super();
+    }
+//}}}
+
+//{{{ empty_code_segment
+//##############################################################################
+//}}}
+
+//{{{ Main, main
+//##############################################################################
+    /**
+    * Main() function for running as an application
+    */
+    public void Main()
+    {
+    }
+
+    public static void main(String[] args)
+    {
+        |CLASSNAME| mainprog = new |CLASSNAME|();
+        try
+        {
+            mainprog.parseArguments(args);
+            mainprog.Main();
+        }
+        catch(IllegalArgumentException ex)
+        {
+            ex.printStackTrace();
+            System.err.println();
+            mainprog.showHelp(true);
+            System.err.println();
+            System.err.println("*** Error parsing arguments: "+ex.getMessage());
+            System.exit(1);
+        }
+    }
+//}}}
+
+//{{{ parseArguments, showHelp
+//##############################################################################
+    /**
+    * Parse the command-line options for this program.
+    * @param args the command-line options, as received by main()
+    * @throws IllegalArgumentException if any argument is unrecognized, ambiguous, missing
+    *   a required parameter, has a malformed parameter, or is otherwise unacceptable.
+    */
+    void parseArguments(String[] args)
+    {
+        String  arg, flag, param;
+        boolean interpFlags = true;
+        
+        for(int i = 0; i < args.length; i++)
+        {
+            arg = args[i];
+            if(!arg.startsWith("-") || !interpFlags || arg.equals("-"))
+            {
+                // This is probably a filename or something
+                interpretArg(arg);
+            }
+            else if(arg.equals("--"))
+            {
+                // Stop treating things as flags once we find --
+                interpFlags = false;
+            }
+            else
+            {
+                // This is a flag. It may have a param after the = sign
+                int eq = arg.indexOf('=');
+                if(eq != -1)
+                {
+                    flag    = arg.substring(0, eq);
+                    param   = arg.substring(eq+1);
+                }
+                else
+                {
+                    flag    = arg;
+                    param   = null;
+                }
+                
+                try { interpretFlag(flag, param); }
+                catch(NullPointerException ex)
+                { throw new IllegalArgumentException("'"+arg
+                    +"' expects to be followed by a parameter"); }
+            }
+        }//for(each arg in args)
+    }
+    
+    // Display help information
+    void showHelp(boolean showAll)
+    {
+        if(showAll)
+        {
+            InputStream is = getClass().getResourceAsStream("|CLASSNAME|.help");
+            if(is == null)
+                System.err.println("\n*** Unable to locate help information in '|CLASSNAME|.help' ***\n");
+            else
+            {
+                try { streamcopy(is, System.out); }
+                catch(IOException ex) { ex.printStackTrace(); }
+            }
+        }
+        System.err.println("|PACKAGE|.|CLASSNAME|");
+        System.err.println("Copyright (C) |YEAR| by Ian W. Davis. All rights reserved.");
+    }
+
+    // Copies src to dst until we hit EOF
+    void streamcopy(InputStream src, OutputStream dst) throws IOException
+    {
+        byte[] buffer = new byte[2048];
+        int len;
+        while((len = src.read(buffer)) != -1) dst.write(buffer, 0, len);
+    }
+//}}}
+
+//{{{ interpretArg, interpretFlag
+//##############################################################################
+    void interpretArg(String arg)
+    {
+        // Handle files, etc. here
+    }
+    
+    void interpretFlag(String flag, String param)
+    {
+        if(flag.equals("-help") || flag.equals("-h"))
+        {
+            showHelp(true);
+            System.exit(0);
+        }
+        else if(flag.equals("-dummy_option"))
+        {
+            // handle option here
+        }
+        else throw new IllegalArgumentException("'"+flag+"' is not recognized as a valid flag");
+    }
+//}}}
+}//class
+
