6b283de358f7dc51cd323b80158c27de48e06c84
iwd <iwd@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2005-01-04 20:22:07 +0000

    - edited Jama files to remove Javadoc errors

--- trunk/javadev/driftwood/src/Jama/CholeskyDecomposition.java
+++ trunk/javadev/driftwood/src/Jama/CholeskyDecomposition.java
@@ -36,8 +36,8 @@ public class CholeskyDecomposition implements java.io.Serializable {
  * ------------------------ */
 
    /** Cholesky algorithm for symmetric and positive definite matrix.
-   @param  A   Square, symmetric matrix.
-   @return     Structure to access L and isspd flag.
+   * Returns structure to access L and isspd flag.
+   @param  Arg   Square, symmetric matrix.
    */
 
    public CholeskyDecomposition (Matrix Arg) {
