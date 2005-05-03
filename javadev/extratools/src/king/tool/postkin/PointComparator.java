29c7106d7e5540fd3bca881771aef28bae1ab215
vbc3 <vbc3@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2005-05-03 22:54:03 +0000

    Change to PointComparator:  - Now sorts points according to atom type also.  - Added comments!

--- javadev/extratools/src/king/tool/postkin/PointComparator.java
+++ javadev/extratools/src/king/tool/postkin/PointComparator.java
@@ -10,7 +10,20 @@ import driftwood.util.*;
 //import chiropraxis.kingtools.*;
 import java.util.*;
 import java.io.*;
-
+/**
+* <code>PointComparator</code> is a comparator used to compare AbstractPoints and determine
+* which would come first, as dictated by PDB format.  This is used in the KinFudgerTool
+* and was originally made to export PDBs from the small peptides used for Ramachandran plots.
+* 
+* Its sorting function is based on the idea of Radix sort, which sorts based on the least
+* significant digit first.  In this case, the points should be sorted by atom name first,
+* then residue name, and then residue number.  Naturally, this comparator is extremely dependent 
+* on the format of the pointID; slight differences in the ID will make this code cry like a little 
+* schoolgirl.
+*
+* <p>Copyright (C) 2005 by Vincent Chen. All rights reserved.
+* 
+*/
 public class PointComparator implements Comparator {
 
 //{{{ Constants
@@ -21,37 +34,71 @@ public class PointComparator implements Comparator {
 //##############################################################################    
     //String[] atomNames = {" N  ", " CA ", " C  ", " O  ", " CB ", " CG ", " CD ", " CE ", " NZ "};
     Props     scProps;
+    ArrayList heteroAtoms;
+    ArrayList hydrogens;
+    ArrayList allAtoms;
 //}}}
 
-
+//{{{ Constructor(s)
+//##################################################################################################
+    /**
+    * Constructor
+    */
     public PointComparator() {
+	
+	heteroAtoms = new ArrayList();
+	hydrogens = new ArrayList();
 	extractAAs();
+	heteroAtoms.addAll(hydrogens);
+	allAtoms = heteroAtoms;
+	//System.out.println(allAtoms);
     }
 
+//{{{ compare
+//##################################################################################################
+    /**
+     * Compares two objects, and if they are abstract points, returns a value according to 
+     * specification given for Comparator.
+     */
     public int compare(Object o1, Object o2) {
 	int value = 0;
 	if ((o1 instanceof AbstractPoint) && (o2 instanceof AbstractPoint)) {
 	    AbstractPoint point1 = (AbstractPoint) o1;
 	    AbstractPoint point2 = (AbstractPoint) o2;
-	    String p1name = point1.getName().toUpperCase().trim();
-	    String p2name = point2.getName().toUpperCase().trim();
+	    String p1name = point1.getName().toUpperCase();
+	    String p2name = point2.getName().toUpperCase();
 	    //value = Integer.parseInt(p1name.substring(8)) - Integer.parseInt(p2name.substring(8));
-	    double calc = point1.getX()-point2.getX()+point1.getY()-point2.getY()+point1.getZ()-point2.getZ();
-	    if (calc == 0) {
-		value = 0;
-	    } else {
-		value = (int)(calc/Math.abs(calc));
-	    }
-	    value = value + getResAA(p1name).compareTo(getResAA(p2name)) * 1000;
+	    String atom1 = p1name.substring(0, 4);
+	    String atom2 = p2name.substring(0, 4);
+	    //System.out.println(atom1 + ", " + atom2);
+	    //double calc = point1.getX()-point2.getX()+point1.getY()-point2.getY()+point1.getZ()-point2.getZ();
+	    //if (calc == 0) {
+	    //	value = 0;
+	    //} else {
+	    //	value = ((int)(calc/Math.abs(calc)));
+	    //}
+
+	    int atomPosition = allAtoms.indexOf(atom1) - allAtoms.indexOf(atom2);
+	    value = atomPosition * 1000;
+
+	    
+	    value = value + getResAA(p1name).compareTo(getResAA(p2name)) * 1000000;
 	
 	    
-	    value = value + (getResNumber(p1name) - getResNumber(p2name)) * 100000;
+	    value = value + (getResNumber(p1name) - getResNumber(p2name)) * 1000000000;
 	    //return value;
+	    if (value == 0) {
+		System.out.println(p1name + ", " + p2name + ": " + value);
+	    }
 	}
 	return value;
     }
 
-
+//{{{ get functions
+//##################################################################################################
+    /**
+     * Functions that extract various info from pointIDs.
+     */
     public int getResNumber(String name) {
 	//String name = point.getName().trim();
 	String[] uncleanParsed = name.split(" ");
@@ -122,6 +169,12 @@ public class PointComparator implements Comparator {
 	}
     }
 
+//{{{ extractAAs
+//##################################################################################################
+    /**
+     * Loads atom connect info from sc-connect.props.
+     *
+     */
     public void extractAAs() {
 	// Load side chain connectivity database
         scProps = new Props();
@@ -137,13 +190,62 @@ public class PointComparator implements Comparator {
         }
         catch(IOException ex)
         { ex.printStackTrace(SoftLog.err); }
+	//scProps.list(System.out);
 
 	Set scs = scProps.keySet();
-	Iterator iter = scs.iterator();
+	Iterator iter = (new TreeSet(scs)).iterator();
 	while (iter.hasNext()) {
 	    String key = (String) iter.next();
-	    System.out.println(key);
+	    if (key.indexOf(".hy") > 0) {
+		buildAALists(hydrogens, scProps.getString(key, ""), "hy");
+	    } else {
+		buildAALists(heteroAtoms, scProps.getString(key, ""), "hetero");
+	    }
+	    //System.out.println(key);
 	}
+	
+    }
+
+
+//{{{ buildAALists
+//##################################################################################################
+    /**
+     * Figures out the atom info from the property file.  I use it to create two arraylists, one for
+     * hydrogens, and one for everything else.  Then, I combine the arraylists into an all atom lists.
+     * The lists should only have each atom type once.  
+     */
+    public void buildAALists(ArrayList list, String connectivity, String type) {
+	String          token = "";
+	boolean  nextToken = false;
+        StringTokenizer tokenizer   = new StringTokenizer(connectivity, ",;", true);
+
+	if (type.equals("hy")) {
+	    while(tokenizer.hasMoreTokens()) {
+		token = tokenizer.nextToken();
+		if (nextToken) {
+		    if (!(list.contains(token))) {
+			//System.out.println(token);
+			list.add(token);
+		    }
+		    nextToken = false;
+		}
+		if (token.equals(",")) {
+		    nextToken = true;
+		}
+
+	    }
+	} else {
+	    while (tokenizer.hasMoreTokens()) {
+		token = tokenizer.nextToken();
+		if (!(token.equals(",")||(token.equals(";")))) {
+		    if (!(list.contains(token))) {
+			//System.out.println(token);
+			list.add(token);
+		    }
+		}
+	    }
+	}
+	//System.out.println(list);
     }
 
 }
