5529c49ebe70b3547f1210d3bbefada79b1348f7
vbc3 <vbc3@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2006-01-10 01:50:21 +0000

    Changes to FramerTool:  - Tool can export calculated values to a file.  - Needs more testing, seemed like a good place to commit.

--- trunk/javadev/extratools/src/king/tool/loops/Framer.java
+++ trunk/javadev/extratools/src/king/tool/loops/Framer.java
@@ -6,12 +6,14 @@ import king.*;
 import king.core.*;
 
 import java.util.*;
+import java.text.DecimalFormat;
+//import java.awt.*;
 import driftwood.r3.*;
 
 public class Framer {
 
 //{{{ Constants
-    
+    static final DecimalFormat df = new DecimalFormat("0.000");
 //}}}
 
 //{{{ Variable definitions
@@ -32,13 +34,13 @@ public class Framer {
 	Triple tripco1 = new Triple(co1);
 	Triple tripcoN = new Triple(coN);
 	//System.out.println(tripca0.distance(ca1));
-	System.out.println(tripca1.distance(caN));
+	//System.out.println(tripca1.distance(caN));
 	//System.out.println(tripcaN.distance(caN1));
-	System.out.println(Triple.angle(tripca0, tripca1, tripcaN));
-	System.out.println(Triple.angle(tripca1, tripcaN, tripcaN1));
-	System.out.println(Triple.dihedral(tripco1, tripca0, tripca1, tripcaN));
-	System.out.println(Triple.dihedral(tripca0, tripca1, tripcaN, tripcaN1));
-	System.out.println(Triple.dihedral(tripca1, tripcaN, tripcaN1, tripcoN));
+	//System.out.println(Triple.angle(tripca0, tripca1, tripcaN));
+	//System.out.println(Triple.angle(tripca1, tripcaN, tripcaN1));
+	//System.out.println(Triple.dihedral(tripco1, tripca0, tripca1, tripcaN));
+	//System.out.println(Triple.dihedral(tripca0, tripca1, tripcaN, tripcaN1));
+	//System.out.println(Triple.dihedral(tripca1, tripcaN, tripcaN1, tripcoN));
 	double[] params = new double[6];
 	params[0] = tripca1.distance(caN);
 	params[1] = Triple.angle(tripca0, tripca1, tripcaN);
@@ -46,7 +48,23 @@ public class Framer {
 	params[3] = Triple.dihedral(tripco1, tripca0, tripca1, tripcaN);
 	params[4] = Triple.dihedral(tripca0, tripca1, tripcaN, tripcaN1);
 	params[5] = Triple.dihedral(tripca1, tripcaN, tripcaN1, tripcoN);
+	System.out.println(df.format(params[0]));
+	System.out.println(df.format(params[1]));
+	System.out.println(df.format(params[2]));
+	System.out.println(df.format(params[3]));
+	System.out.println(df.format(params[4]));	
+	System.out.println(df.format(params[5]));
 	return params;
     }
+
+    public static ArrayList calphaAnalyzeList(KPoint ca0, KPoint ca1, KPoint caN, KPoint caN1, KPoint co1, KPoint coN) {
+	double[] results = calphaAnalyze(ca0, ca1, caN, caN1, co1, coN);
+	ArrayList list = new ArrayList();
+	for (int i = 0; i < results.length; i++) {
+	    list.add(new Double(results[i]));
+	}
+	return list;
+    }
+
 }
 	//Triple tripca1 = new Triple(ca1);
