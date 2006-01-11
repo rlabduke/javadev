66dea0e32dfa72304d442dbcae15b55fd6aace09
vbc3 <vbc3@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2006-01-11 16:31:43 +0000

    LoopDockingTool:  - Fixed NullPointerException caused from changing the amount of structure     cropped in automated extraction part of the LoopTool.
    FramerTool:
     - Fixed serious error in the calculations; have to use oxygen of residue 0,
        not of residue 1, for calculating the dihedral.

--- javadev/extratools/src/king/tool/loops/Framer.java
+++ javadev/extratools/src/king/tool/loops/Framer.java
@@ -26,12 +26,12 @@ public class Framer {
     }
 
 
-    public static double[] calphaAnalyze(KPoint ca0, KPoint ca1, KPoint caN, KPoint caN1, KPoint co1, KPoint coN) {
+    public static double[] calphaAnalyze(KPoint ca0, KPoint ca1, KPoint caN, KPoint caN1, KPoint co0, KPoint coN) {
 	Triple tripca0 = new Triple(ca0);
 	Triple tripca1 = new Triple(ca1);
 	Triple tripcaN = new Triple(caN);
 	Triple tripcaN1 = new Triple(caN1);
-	Triple tripco1 = new Triple(co1);
+	Triple tripco0 = new Triple(co0);
 	Triple tripcoN = new Triple(coN);
 	//System.out.println(tripca0.distance(ca1));
 	//System.out.println(tripca1.distance(caN));
@@ -45,20 +45,20 @@ public class Framer {
 	params[0] = tripca1.distance(caN);
 	params[1] = Triple.angle(tripca0, tripca1, tripcaN);
 	params[2] = Triple.angle(tripca1, tripcaN, tripcaN1);
-	params[3] = Triple.dihedral(tripco1, tripca0, tripca1, tripcaN);
+	params[3] = Triple.dihedral(tripco0, tripca0, tripca1, tripcaN);
 	params[4] = Triple.dihedral(tripca0, tripca1, tripcaN, tripcaN1);
 	params[5] = Triple.dihedral(tripca1, tripcaN, tripcaN1, tripcoN);
-	System.out.println(df.format(params[0]));
-	System.out.println(df.format(params[1]));
-	System.out.println(df.format(params[2]));
-	System.out.println(df.format(params[3]));
-	System.out.println(df.format(params[4]));	
+	System.out.print(df.format(params[0]) + " ");
+	System.out.print(df.format(params[1]) + " ");
+	System.out.print(df.format(params[2]) + " ");
+	System.out.print(df.format(params[3]) + " ");
+	System.out.print(df.format(params[4]) + " ");	
 	System.out.println(df.format(params[5]));
 	return params;
     }
 
-    public static ArrayList calphaAnalyzeList(KPoint ca0, KPoint ca1, KPoint caN, KPoint caN1, KPoint co1, KPoint coN) {
-	double[] results = calphaAnalyze(ca0, ca1, caN, caN1, co1, coN);
+    public static ArrayList calphaAnalyzeList(KPoint ca0, KPoint ca1, KPoint caN, KPoint caN1, KPoint co0, KPoint coN) {
+	double[] results = calphaAnalyze(ca0, ca1, caN, caN1, co0, coN);
 	ArrayList list = new ArrayList();
 	for (int i = 0; i < results.length; i++) {
 	    list.add(new Double(results[i]));
