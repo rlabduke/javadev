f7efe377343179f6afabd39a0ba3871ef3f99d41
iwd <iwd@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2006-02-10 18:44:25 +0000

    - fixed PdbSecondaryStructure to catch IndexOutOfBoundsException for short lines

--- trunk/javadev/driftwood/src/driftwood/moldb2/PdbSecondaryStructure.java
+++ trunk/javadev/driftwood/src/driftwood/moldb2/PdbSecondaryStructure.java
@@ -36,70 +36,65 @@ class PdbSecondaryStructure extends SecondaryStructure
         for(Iterator iter = headers.iterator(); iter.hasNext(); )
         {
             String s = (String) iter.next();
-                 if(s.startsWith("HELIX ")) ranges.add(forHelix(s));
-            else if(s.startsWith("SHEET ")) ranges.add(forSheet(s));
-            else if(s.startsWith("TURN  ")) ranges.add(forTurn(s));
+            try
+            {
+                     if(s.startsWith("HELIX ")) ranges.add(forHelix(s));
+                else if(s.startsWith("SHEET ")) ranges.add(forSheet(s));
+                else if(s.startsWith("TURN  ")) ranges.add(forTurn(s));
+            }
+            catch(NumberFormatException ex)
+            { System.err.println("Non-numeric sequence numbers: "+s); }
+            catch(IndexOutOfBoundsException ex)
+            { System.err.println("PDB record too short: "+s); }
         }
     }
 //}}}
 
 //{{{ forHelix, forSheet, forTurn
 //##############################################################################
+    // All of these can throw NumberFormatException (for non-numeric data)
+    // and IndexOutOfBoundsException, for lines that are too short.
+
     /** @param s a PDB "HELIX" record */
-    Range forHelix(String s)
+    Range forHelix(String s) throws NumberFormatException
     {
         Range r = new Range();
         r.type = HELIX;
         r.chainId = s.substring(19,20);
         if(!r.chainId.equals(s.substring(31,32)))
             System.err.println("Mismatched chain IDs: "+s);
-        try
-        {
-            r.initSeqNum = Integer.parseInt(s.substring(21,25).trim());
-            r.endSeqNum  = Integer.parseInt(s.substring(33,37).trim());
-        }
-        catch(NumberFormatException ex)
-        { System.err.println("Non-numeric sequence numbers: "+s); }
+        r.initSeqNum = Integer.parseInt(s.substring(21,25).trim());
+        r.endSeqNum  = Integer.parseInt(s.substring(33,37).trim());
         r.initICode = s.substring(25,26);
         r.endICode  = s.substring(37,38);
         return r;
     }
 
     /** @param s a PDB "SHEET" record */
-    Range forSheet(String s)
+    Range forSheet(String s) throws NumberFormatException
     {
         Range r = new Range();
         r.type = STRAND;
         r.chainId = s.substring(21,22);
         if(!r.chainId.equals(s.substring(32,33)))
             System.err.println("Mismatched chain IDs: "+s);
-        try
-        {
-            r.initSeqNum = Integer.parseInt(s.substring(22,26).trim());
-            r.endSeqNum  = Integer.parseInt(s.substring(33,37).trim());
-        }
-        catch(NumberFormatException ex)
-        { System.err.println("Non-numeric sequence numbers: "+s); }
+        r.initSeqNum = Integer.parseInt(s.substring(22,26).trim());
+        r.endSeqNum  = Integer.parseInt(s.substring(33,37).trim());
         r.initICode = s.substring(26,27);
         r.endICode  = s.substring(37,38);
         return r;
     }
 
     /** @param s a PDB "TURN" record */
-    Range forTurn(String s)
+    Range forTurn(String s) throws NumberFormatException
     {
         Range r = new Range();
         r.type = TURN;
         r.chainId = s.substring(19,20);
         if(!r.chainId.equals(s.substring(30,31)))
             System.err.println("Mismatched chain IDs: "+s);
-        try
-        {
-            r.initSeqNum = Integer.parseInt(s.substring(20,24).trim());
-            r.endSeqNum  = Integer.parseInt(s.substring(31,35).trim());
-        }
-        catch(NumberFormatException ex)
-        { System.err.println("Non-numeric sequence numbers: "+s); }
+        r.initSeqNum = Integer.parseInt(s.substring(20,24).trim());
+        r.endSeqNum  = Integer.parseInt(s.substring(31,35).trim());
         r.initICode = s.substring(24,25);
         r.endICode  = s.substring(35,36);
         return r;
