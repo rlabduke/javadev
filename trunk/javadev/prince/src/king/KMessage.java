e71146e4ae3b5641f989a0f60315070f993b51d4
iwd <iwd@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2006-12-18 19:48:25 +0000

    - KinfileLoader moved into king.io - constants added to KMessage to follow KingMain EV_* constants

--- trunk/javadev/prince/src/king/KMessage.java
+++ trunk/javadev/prince/src/king/KMessage.java
@@ -29,27 +29,58 @@ public class KMessage //extends ... implements ...
     }
     
 //{{{ Constants
+    /** A different kinemage has become the currently active one */
+    public static final long KIN_SWITCHED       = (1L<<0);
+    /** A kinemage (presumably the current one) has been closed */
+    public static final long KIN_CLOSED         = (1L<<1);
+    /** All open kinemages have been closed */
+    public static final long ALL_CLOSED         = (1L<<2);
+    /** The KingPrefs object has been updated */
+    public static final long PREFS_CHANGED      = (1L<<3);
+    /** The current viewpoint has been altered: center moved, zoom changed, rotated, etc */
+    public static final long VIEW_MOVED         = (1L<<4);
+    /** A totally different viewpoint has been selected from the Views menu */
+    public static final long VIEW_SELECTED      = (1L<<5);
 //}}}
 
 //{{{ Variable definitions
 //##############################################################################
-    protected Object source;
+    protected Object    source;
+    protected int       kinChanges;
+    protected long      progChanges;
 //}}}
 
 //{{{ Constructor(s)
 //##############################################################################
     public KMessage(Object source)
+    { this(source, 0, 0); }
+    
+    public KMessage(Object source, int kinChanges, long progChanges)
     {
         super();
-        this.source = source;
+        this.source         = source;
+        this.kinChanges     = kinChanges;
+        this.progChanges    = progChanges;
     }
 //}}}
 
-//{{{ getSource
+//{{{ getSource, get/set{Kinemage, Program}Changes
 //##############################################################################
     /** Returns the "source" or originator of this message. */
     public Object getSource()
     { return this.source; }
+    
+    public int getKinemageChanges()
+    { return this.kinChanges; }
+    
+    public void setKinemageChanges(int c)
+    { this.kinChanges = c; }
+    
+    public long getProgramChanges()
+    { return this.progChanges; }
+    
+    public void setProgramChanges(long c)
+    { this.progChanges = c; }
 //}}}
 
 //{{{ empty_code_segment
