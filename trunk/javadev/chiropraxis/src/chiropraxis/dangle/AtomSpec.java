7efa5c7c35370a9b2dd15aa9e7c6ba4ecd8c15b5
iwd <iwd@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2007-04-26 00:01:37 +0000

    - added "maxb" and "minq" to Dangle

--- trunk/javadev/chiropraxis/src/chiropraxis/dangle/AtomSpec.java
+++ trunk/javadev/chiropraxis/src/chiropraxis/dangle/AtomSpec.java
@@ -16,6 +16,7 @@ import driftwood.moldb2.*;
 * <code>AtomSpec</code> takes strings like "_CA_" and matches
 * them to atom names like " CA ".
 * For nucleic acids, the symbols * and ' are treated as equivalent.
+* Regular expressions can be specified by enclosing the regex in slashes (/.../).
 *
 * <p>Copyright (C) 2007 by Ian W. Davis. All rights reserved.
 * <br>Begun on Thu Feb 15 11:18:34 EST 2007
@@ -58,24 +59,16 @@ public class AtomSpec //extends ... implements ...
     }
 //}}}
 
-//{{{ get, checkConnection
+//{{{ get
 //##############################################################################
     /**
     * @return the AtomState specified by this spec, or null if it can't be found.
     */
     public AtomState get(Model model, ModelState state, Residue curr)
     {
-        Residue res = curr;
-        for(int i = 0, end_i = Math.abs(resOffset); i < end_i; i++)
-        {
-            Residue old = res;
-            if(resOffset > 0) res = curr.getNext(model); // forward search
-            else res = curr.getPrev(model); // backward search
-            if(res == null) return null;
-            
-            if(resOffset > 0)   { if(!checkConnection(model, state, old, res)) return null; }
-            else                { if(!checkConnection(model, state, res, old)) return null; }
-        }
+        Residue res = getRes(model, state, curr);
+        if(res == null) return null;
+        
         Atom atom = null;
         if(regexName == null)
         {
@@ -95,7 +88,65 @@ public class AtomSpec //extends ... implements ...
         try { return state.get(atom); }
         catch(AtomException ex) { return null; }
     }
-    
+//}}}
+
+//{{{ getAll
+//##############################################################################
+    /**
+    * @return all the AtomStates specified by this spec, or the empty set.
+    *   This only makes sense really if the spec is a regular expression.
+    */
+    public Collection getAll(Model model, ModelState state, Residue curr)
+    {
+        Residue res = getRes(model, state, curr);
+        if(res == null) return Collections.emptySet();
+        
+        if(regexName == null)
+        {
+            Atom atom = res.getAtom(starName);
+            if(atom == null) atom = res.getAtom(primeName);
+            if(atom == null) return Collections.emptySet();
+            try { return Collections.singleton(state.get(atom)); }
+            catch(AtomException ex) { return Collections.emptySet(); }
+        }
+        else
+        {
+            ArrayList all = new ArrayList();
+            for(Iterator iter = res.getAtoms().iterator(); iter.hasNext(); )
+            {
+                Atom a = (Atom) iter.next();
+                if(regexName.reset(a.getName()).matches())
+                {
+                    try { all.add(state.get(a)); }
+                    catch(AtomException ex) {}
+                }
+            }
+            return all;
+        }
+    }
+//}}}
+
+//{{{ getRes
+//##############################################################################
+    private Residue getRes(Model model, ModelState state, Residue curr)
+    {
+        Residue res = curr;
+        for(int i = 0, end_i = Math.abs(resOffset); i < end_i; i++)
+        {
+            Residue old = res;
+            if(resOffset > 0) res = curr.getNext(model); // forward search
+            else res = curr.getPrev(model); // backward search
+            if(res == null) return null;
+            
+            if(resOffset > 0)   { if(!checkConnection(model, state, old, res)) return null; }
+            else                { if(!checkConnection(model, state, res, old)) return null; }
+        }
+        return res;
+    }
+//}}}
+
+//{{{ checkConnection
+//##############################################################################
     private boolean checkConnection(Model model, ModelState state, Residue first, Residue second)
     {
         Atom c = null, n = null, o = null, p = null;
