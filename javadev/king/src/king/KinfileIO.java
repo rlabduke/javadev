13878a1e631201ad63c4d2f9e1b90d4ddc2cc578
iwd <iwd@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2007-08-22 21:16:26 +0000

    - added workarounds to JFileChooser creation bug in unsigned applets   under Java 6 on Windows (Sun bug # 6570445)

--- javadev/king/src/king/KinfileIO.java
+++ javadev/king/src/king/KinfileIO.java
@@ -108,6 +108,14 @@ public class KinfileIO implements KinfileLoader.Listener, ListSelectionListener
             }
         }
         catch(SecurityException ex) {}
+        // Temporary fix for Java 6 bug # 6570445:  JFileChooser in unsigned applet
+        catch(java.lang.ExceptionInInitializerError ex)
+        {
+            if(!(ex.getCause() instanceof java.security.AccessControlException))
+                throw ex;
+        }
+        // Subsequent attempts to create JFileChooser cause NoClassDefFound
+        catch(java.lang.NoClassDefFoundError ex) {}
     }
 //}}}
 
