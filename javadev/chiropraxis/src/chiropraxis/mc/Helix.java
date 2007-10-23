f7c1f3f57023d02ba1e9baa64c67693548107cde
keedy <keedy@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2007-10-23 16:21:12 +0000

    ### 0.80 ### (dak) - Added new Helix class to fxn w/ HelixBuilder - Working on Ncap-finding method (incomplete...) in HelixBuilder

---
+++ javadev/chiropraxis/src/chiropraxis/mc/Helix.java
@@ -0,0 +1,93 @@
+// (jEdit options) :folding=explicit:collapseFolds=1:
+//{{{ Package, imports
+package chiropraxis.mc;
+
+//import java.awt.*;
+//import java.awt.event.*;
+import java.io.*;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.*;
+//import java.util.regex.*;
+//import javax.swing.*;
+import driftwood.moldb2.*;
+import driftwood.r3.*;
+//}}}
+/**
+* <code>Helix</code> is a class evolved from Ian's Peptide.
+* HelixBuilder makes an array of these then does alignments and whatnot to them..
+* At the moment, all fields are public and written to directly.
+*
+* <p>Copyright (C) 2007 by Daniel Keedy. All rights reserved.
+* <br>Begun on Tue Mar 30 10:45:56 EST 2004
+*/
+public class Helix //extends ... implements ...
+{
+//{{{ Constants
+//}}}
+
+//{{{ Variable definitions
+//##############################################################################
+    /** Residues in this helix. Inherently contains chain and resnum info. */
+    public ArrayList<Residue> residues;
+    
+    /** PDB code for the structure this helix is in */
+    public String pdb;
+    
+    /** Residue which is the Ncap for this helix. */
+    public Residue ncap;
+    
+//}}}
+
+//{{{ Constructor(s)
+//##############################################################################
+    public Helix(TreeSet<Residue> residuesSet)
+    {
+        super();
+        Iterator iter = residuesSet.iterator();
+        residues = new ArrayList<Residue>();
+        while (iter.hasNext())
+            residues.add( (Residue)iter.next() );
+        Collections.sort(residues);
+        ncap = null;
+    }
+//}}}
+
+//{{{ toString
+//##############################################################################
+    public String toString()
+    {
+        return "helix from "+getRes("first")+" to "+getRes("last");
+    }
+//}}}
+
+//{{{ getRes
+//##############################################################################
+    public Residue getRes(String firstOrLast)
+    {
+        Residue resToReturn = residues.get(0); 
+        if (firstOrLast.equals("first"))
+        {
+            for (Residue res : residues)
+            {
+                if (res.getSequenceInteger() < resToReturn.getSequenceInteger())
+                    resToReturn = res;
+            }
+        }
+        else if (firstOrLast.equals("last"))
+        {
+            for (Residue res : residues)
+            {
+                if (res.getSequenceInteger() > resToReturn.getSequenceInteger())
+                    resToReturn = res;
+            }
+        }
+        return resToReturn;
+    }
+//}}}
+
+//{{{ empty_code_segment
+//##############################################################################
+//}}}
+}//class
+
