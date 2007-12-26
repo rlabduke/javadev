b795a4eacbe3b3fc8060e4827dd07ff0626820cf
vbc3 <vbc3@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2007-12-26 22:25:33 +0000

    Cleaned up tabs and made Framer classes more uniform. Added axis to RdcPlayPlugin. Changes in cmdline:  -Added new function for creating protein fragment 7 param libraries.   Includes a few extra parameters now for looking at the first and last two   peptides of a fragment.

--- trunk/javadev/fragmentfiller/src/fragmentfiller/Framer.java
+++ trunk/javadev/fragmentfiller/src/fragmentfiller/Framer.java
@@ -20,7 +20,7 @@ public class Framer {
   //{{{ Variable definitions
   //######################################################
   
-  
+  //}}}
   
   //{{{ Constructor
   public Framer() {
@@ -28,6 +28,7 @@ public class Framer {
   }
   //}}}  
 
+  //{{{ calphaAnalyze
   public static double[] calphaAnalyze(Tuple3 ca0, Tuple3 ca1, Tuple3 caN, Tuple3 caN1, Tuple3 co0, Tuple3 coN) {
     Triple tripca0 = new Triple(ca0);
     Triple tripca1 = new Triple(ca1);
@@ -58,16 +59,10 @@ public class Framer {
     //System.out.println(df.format(params[5]));
     return params;
   }
+  //}}}
   
+  //{{{ frameAnalyze
   public static double[] frameAnalyze(Triple tripca0, Triple tripca1, Triple tripcaN, Triple tripcaN1, Triple tripco0, Triple tripcoN) {
-    //System.out.println(tripca0.distance(ca1));
-    //System.out.println(tripca1.distance(caN));
-    //System.out.println(tripcaN.distance(caN1));
-    //System.out.println(Triple.angle(tripca0, tripca1, tripcaN));
-    //System.out.println(Triple.angle(tripca1, tripcaN, tripcaN1));
-    //System.out.println(Triple.dihedral(tripco1, tripca0, tripca1, tripcaN));
-    //System.out.println(Triple.dihedral(tripca0, tripca1, tripcaN, tripcaN1));
-    //System.out.println(Triple.dihedral(tripca1, tripcaN, tripcaN1, tripcoN));
     double[] params = new double[6];
     params[0] = tripca1.distance(tripcaN);
     params[1] = Triple.angle(tripca0, tripca1, tripcaN);
@@ -83,7 +78,9 @@ public class Framer {
     //System.out.println(df.format(params[5]));
     return params;
   }
-
+  //}}}
+  
+  //{{{ calphaAnalyzeList
   public static ArrayList calphaAnalyzeList(Tuple3 ca0, Tuple3 ca1, Tuple3 caN, Tuple3 caN1, Tuple3 co0, Tuple3 coN) {
     double[] results = calphaAnalyze(ca0, ca1, caN, caN1, co0, coN);
     ArrayList list = new ArrayList();
@@ -92,6 +89,6 @@ public class Framer {
     }
     return list;
   }
+  //}}}
   
 }
-//Triple tripca1 = new Triple(ca1);
