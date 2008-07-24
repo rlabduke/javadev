ea6f5caeef564f884aeb9e9e3008e7ef6b75f843
vbc3 <vbc3@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2008-07-24 18:29:14 +0000

    new code for exploring parameter space, and starting on betas

--- javadev/fragmentfiller/src/absigner/Parameters.java
+++ javadev/fragmentfiller/src/absigner/Parameters.java
@@ -14,6 +14,8 @@ public class Parameters {
   //{{{ Variables
   ArrayList<Parameter> alphaParams;
   ArrayList<Parameter> alphaSD;
+  ArrayList<Parameter> betaParams;
+  ArrayList<Parameter> betaSD;
   //}}}
   
   //{{{ class: Parameter
@@ -36,13 +38,17 @@ public class Parameters {
     alphaParams = new ArrayList<Parameter>();
     alphaSD = new ArrayList<Parameter>();
     Parameter alpha1 = new Parameter(   3.788, 91.2, 92.2, -74, 51, 125);
-    Parameter alpha1sd = new Parameter( 0.2  ,  5  ,  5  ,   5,  4,   5);
+    Parameter alpha1sd = new Parameter( 0.1  ,  1  ,  1  ,   1,  1,   1);
     Parameter alpha2 = new Parameter(5.4, 66, 62, -37, 113, 160.5);
-    Parameter alpha2sd = new Parameter(0.4, 5, 5, 6, 6, 6);
+    Parameter alpha2sd = new Parameter(0.1, 1, 1, 1, 1, 1);
     alphaParams.add(alpha1);
     alphaParams.add(alpha2);
     alphaSD.add(alpha1sd);
     alphaSD.add(alpha2sd);
+    Parameter beta2 = new Parameter(    6.357,   161.6,   167,   -124.6,   12.8,  -31.9   );
+    Parameter beta2sd = new Parameter(  0.1,       1,       1,      1,      1,      1     );
+    betaParams.add(beta2);
+    betaSD.add(beta2sd);
   }
   //}}}
 
@@ -70,5 +76,60 @@ public class Parameters {
   }
   //}}}
   
+  //{{{ getBetaSize
+  public double[] getBetaSize(int size) {
+    if (size <= betaParams.size()) {
+      Parameter param = betaParams.get(size-1);
+      return param.get();
+    } else {
+      System.err.println("No data on beta fragments of that size available!");
+      return null;
+    }
+  }
+  //}}}
   
+  //{{{ getBetaSD
+  public double[] getBetaSD(int size) {
+    if (size <= betaSD.size()) {
+      Parameter param = betaSD.get(size-1);
+      return param.get();
+    } else {
+      System.err.println("No data on beta standard dev of that size available!");
+      return null;
+    }
+  }
+  //}}}
+  
+  //{{{ inRange
+  public static boolean inRange(double[] currFrag, double[] ssParams, double[] paramsSD) {
+    if ((currFrag[0] > ssParams[0] + paramsSD[0]) || (currFrag[0] < ssParams[0] - paramsSD[0])) {
+      return false;
+    }
+    for (int i = 1; i < ssParams.length; i++) {
+      if (angleInRange(currFrag[i], ssParams[i], paramsSD[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+  //}}}
+  
+  //{{{ angleInRange
+  public static boolean angleInRange(double value, double param, double sd) {
+    if (param > 180 - sd) {
+      if (!((value >= param - sd) || (value <= -360 + param + sd))) {
+        return false;
+      }
+    } else if (param < -180 + sd) {
+      if (!((value <= param + sd) || (value >= 360 + param - sd))) {
+        return false;
+      }
+    } else {
+      if ((value > param + sd) || (value < param + sd)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  //}}}
 }
