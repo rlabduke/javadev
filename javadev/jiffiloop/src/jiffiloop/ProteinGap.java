47a0453dc2bedc2d1634c33996ec054879a14954
vbc3 <vbc3@048b549b-a3d7-0310-93d3-e3c4d06e1c1f>
2008-11-18 16:23:50 +0000

    Removed some system.outs and changed output to spit out score for each "gap" in jiffiloop.

--- javadev/jiffiloop/src/jiffiloop/ProteinGap.java
+++ javadev/jiffiloop/src/jiffiloop/ProteinGap.java
@@ -51,11 +51,11 @@ public class ProteinGap {
       states.add(co0);
       states.add(coN);
       frame = Framer.calphaAnalyzeList(ca0, ca1, caN, caN1, co0, coN);
-      System.out.print(sourceModName + " " + sourceChain + " ");
-      for (double d : frame) {
-        System.out.print(df.format(d) + " ");
-      }
-      System.out.println();
+      //System.out.print(sourceModName + " " + sourceChain + " ");
+      //for (double d : frame) {
+      //  System.out.print(df.format(d) + " ");
+      //}
+      //System.out.println();
     } catch (AtomException ae) {
       System.err.println("Problem with atom " + ae.getMessage());
     }
